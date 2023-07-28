2c0ed24d017587a672d62e079fcfdd374eaf3a2f
EunicePMV <villanueva.eunice.m@gmail.com>
2023-07-28 23:40:23 +0800

    static files

---
+++ staticfiles/I_course/I_course_mark.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const csrftoken = getCookie('csrftoken');
+    studentScores = document.querySelectorAll('.score-input');
+    studentScores.forEach(studentScore => {
+        studentScore.addEventListener('change', function(e) {
+            stringId = studentScore.className.slice(11, 19).trim();
+            id = stringId.split("-")
+
+            const grade = studentScore.value;
+            // console.log(id);
+            console.log(`submission/${id[0]}/${id[1]}/${id[2]}`);
+
+            // get student.id course.id and task.id
+            fetch(`submission/${id[0]}/${id[1]}/${id[2]}`, {
+                method: 'PUT',
+                headers: {'X-CSRFTokesubmissionn': csrftoken},
+                body: JSON.stringify({
+                    grade: grade
+                })
+            })
+        });
+    });
+});
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
